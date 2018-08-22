a9d417cf4cd65ffd069227bb8fa4c5aca5292284
Bjørn Erik Pedersen <bjorn.erik.pedersen@gmail.com>
2018-08-22 09:18:32 +0200

    Make a more complete test case

--- main.go
+++ main.go
@@ -1,37 +1,25 @@
 package main
 
 import (
-	"bytes"
-	"fmt"
+	"io"
+	"log"
+	"os"
 
 	"github.com/russross/blackfriday"
 )
 
 func main() {
-	const md = `
-
-## Heading 1
-
-Some **text**
-
-## Heading 2
-
-This is a footnote.[^fn1]
-
-### Heading 2-1
-
-Some more text.[^fn2]
-
-[^fn1]: the footnote text.
-[^fn2]: the footnote 2 text.
-
-`
-	run(md)
+	f, err := os.Create("/Users/bep/sites/dump/bf.html")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer f.Close()
+	run(md, f)
 
 	//fmt.Println("BlackFriday v2 Test:\n", string(output))
 }
 
-func run(input string, opts ...blackfriday.Option) []byte {
+func run(input string, out io.Writer, opts ...blackfriday.Option) {
 	r := blackfriday.NewHTMLRenderer(blackfriday.HTMLRendererParameters{
 		Flags: blackfriday.CommonHTMLFlags,
 	})
@@ -42,12 +30,50 @@ func run(input string, opts ...blackfriday.Option) []byte {
 	optList = append(optList, opts...)
 	parser := blackfriday.New(optList...)
 	ast := parser.Parse([]byte(input))
-	var buf bytes.Buffer
-	r.RenderHeader(&buf, ast)
+	r.RenderHeader(out, ast)
 	ast.Walk(func(node *blackfriday.Node, entering bool) blackfriday.WalkStatus {
-		fmt.Printf("%d:%d:%s:%v\t\t%s\n", node.Level, node.NoteID, node.Destination, node.Type, node.String())
-		return r.RenderNode(&buf, node, entering)
+		//fmt.Printf("%d:%d:%s:%v\t\t%s\n", node.Level, node.NoteID, node.Destination, node.Type, node.String())
+		return r.RenderNode(out, node, entering)
 	})
-	r.RenderFooter(&buf, ast)
-	return buf.Bytes()
+	r.RenderFooter(out, ast)
 }
+
+// Hugo custom
+// BlockCode (code highlighting) => Node type Code
+// ListItem, List (todo)
+//
+
+type RenderModifier interface {
+}
+
+const md = `
+
+## Heading 1
+
+Some **text**.
+
+## Heading 2
+
+This is a footnote.[^fn1]
+
+A fenced code block:
+
+` + "```" + `go
+type RenderModifier interface {
+}
+` + "```" + `
+
+### Heading 2-1
+
+Task list:
+
+- [x] Finish my changes
+- [ ] Push my commits to GitHub
+- [ ] Open a pull request
+
+Some more text.[^fn2]
+
+[^fn1]: the footnote text.
+[^fn2]: the footnote 2 text.
+
+`
